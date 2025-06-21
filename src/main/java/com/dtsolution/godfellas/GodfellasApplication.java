3cec7e4793598a08afcfa002cb4fa4d176f3d449
PM_User <danuka@paymedia.lk>
2025-06-21 14:33:13 +0530

    init

---
+++ src/main/java/com/dtsolution/godfellas/GodfellasApplication.java
@@ -0,0 +1,13 @@
+package com.dtsolution.godfellas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodfellasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodfellasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtsolution/godfellas/GodfellasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtsolution.godfellas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodfellasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
